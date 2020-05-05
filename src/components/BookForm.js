a575675e08044f0191145781e135c62b89280673
kharioki <antolisah@gmail.com>
2020-05-06 00:21:25 +0300

    add book form and form styles

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,7 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
 
 function BookForm() {
-  return <div></div>;
+  const { addBook } = useContext(BookContext);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleAddBook = e => {
+    e.preventDefault();
+    addBook(title, author);
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <form onSubmit={handleAddBook}>
+      <input
+        type="text"
+        placeholder="Book title"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        value={author}
+        onChange={e => setAuthor(e.target.value)}
+        required
+      />
+      <input type="submit" value="Add Book" />
+    </form>
+  );
 }
 
 export default BookForm;
